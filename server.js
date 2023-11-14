cc3a261a76a96f8ab313365df56bfdad7f5be36b
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2023-11-13 22:08:43 -0600

    edit: fix spacing, add semi

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-const Fruit = require('./models/fruit.js')
+const Fruit = require('./models/fruit.js');
 
 app.get('/', async (req, res) => {
   res.render('index.ejs');
 });
 
-app.get('/fruits/new', (req, res)=>{
+app.get('/fruits/new', (req, res) => {
   res.render('fruits/new.ejs');
 });
 
